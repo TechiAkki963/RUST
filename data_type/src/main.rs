dbae5981183d35d202ab7a484aa57f9845df16de
Akshay Parab <paraba777@gmail.com>
2025-03-09 18:09:04 +0530

    Boolean and Inversion

--- data_type/src/main.rs
+++ data_type/src/main.rs
@@ -51,15 +51,15 @@ fn main() {
     // * floating Point
 
     let pi: f64 = 3.14159;
-    println!("The value of pi {}",pi); 
+    println!("The value of pi {}", pi);
 
-    println!("{}",pi.floor());  //3
-    println!("{}",pi.ceil());   //4
-    println!("{}",pi.round());
+    println!("{}", pi.floor()); //3
+    println!("{}", pi.ceil()); //4
+    println!("{}", pi.round());
 
     // * float format specifier
 
-    println!("The current value of pi is {pi:.2}"); //3.14 
+    println!("The current value of pi is {pi:.2}"); //3.14
 
     // * Casting type with "as" keyword
 
@@ -70,10 +70,67 @@ fn main() {
     println!("{miles_away_i8}");
     println!("{miles_away_u8}");
 
-    let miles_away: f64 =  100.641239784654321213115645;
+    let miles_away: f64 = 100.641239784654321213115645;
     let miles_away_f32 = miles_away as f32;
-    let miles_away_i8=miles_away as i8;
+    let miles_away_i8 = miles_away as i8;
     println!("{miles_away_f32}");
     println!("{miles_away_i8}");
+
+    // *Math Operators
+
+    let addition = 5 + 4;
+    let subtraction = 10 - 6;
+    let multiplication = 3 * 4;
+
+    println!(
+        "addition : {} , subtraction : {} , multiplication : {}",
+        addition, subtraction, multiplication
+    );
+
+    let floor_division = 5 / 3;
+    println!("division : {}", floor_division);
+
+    let decimal_division = 5.0 / 3.0;
+    println!("decimal division : {}", decimal_division);
+
+    let reminder = 9 % 2;
+    println!("{}", reminder);
+
+    // *Augmented Operator
+    let mut year: i32 = 2025;
+    // year = year + 1;
+    year += 1;
+    println!("increment : {}", year);
+
+    year -= 2; 
+    println!("decrement : {}", year);
+
+    year *= 3;
+    println!("multiplication : {}", year);
+
+    year /= 5;
+    println!("dividation : {}", year);
+
+    // *Boolean
+
+    let is_handsome = true;
+    let is_bodybuilder = false;
+
+    println!("You are handsome: {} and also Bodybuilder: {}", is_handsome,is_bodybuilder);
+
+    let age: i32 = 21;
+    let is_mature = age > 18;
+    println!("{is_mature}");
+    println!(" Is eleigible to drive : {}", is_mature);
+    println!("Method Positive : {} Method Negative : {}",age.is_negative(),age.is_positive());
+
+    // * Boolean Inversion
+    let can_see_r_rated_movie = age >= 17;
+    let cannot_see_r_rated_movie =  !can_see_r_rated_movie;
+    println!("Your age {}, Eligibility to watch R rated movie : {}", age,can_see_r_rated_movie);
+
+    // *Equality and Inequality
+    
+
 
 }
