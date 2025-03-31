031c5f8bdfaa1c6c6f57d834c3a13e912cd801c2
Akshay Parab <paraba777@gmail.com>
2025-03-31 14:47:22 +0530

    cargo fmt

--- function/src/main.rs
+++ function/src/main.rs
@@ -6,31 +6,30 @@ fn main() {
     serve(3, "chocochips");
     serve_icecream();
 
-    let result =square(5);
+    let result = square(5);
     println!("The square of 5 : {result}");
 
-    let result =square(25);
+    let result = square(25);
     println!("The square of 25: {result}");
 }
 
-
-fn make_cream(fruit: &str){
+fn make_cream(fruit: &str) {
     println!("Making Fruit Creams using {fruit}");
 }
 
-fn base_option(base: &str){
+fn base_option(base: &str) {
     println!("Base Option : {base}");
 }
 
-fn serve(scoops: i8, toppings: &str){
+fn serve(scoops: i8, toppings: &str) {
     println!("No. of Scoops: {scoops}, Toppings:{toppings}")
 }
 
-fn serve_icecream(){
+fn serve_icecream() {
     println!("Serve the icecream");
 }
 
 // *Return Value
-fn square(number: i32) -> i32{
-    return number*number;
+fn square(number: i32) -> i32 {
+    return number * number;
 }
