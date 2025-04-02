42e4d461bb517baecc78e89550f3f6196ffbf802
Akshay Parab <paraba777@gmail.com>
2025-04-02 16:37:27 +0530

    if else variable

--- control/src/main.rs
+++ control/src/main.rs
@@ -3,23 +3,30 @@ fn main() {
         println!("True Output");
     }
 
-    if false{
+    if false {
         println!("False Output")
     }
 
-
     let season: &str = " ";
 
-    if season == "summer"{
+    if season == "summer" {
         println!("It's Summer, Clouds are clear");
-    } else if season == "winter"{
+    } else if season == "winter" {
         println!("It's Winter, I am freezing!!!");
     } else if season == "rainy" {
         println!("Oooooh its thundering")
     } else if season == "spring" {
         println!("W@@@@W! flower blooming!!!");
-    }
-    else{
+    } else {
         println!("Suprisingly raining......");
     }
+
+    even_or_odd(20);
+    even_or_odd(17);
+}
+
+fn even_or_odd(number: i32) {
+    let result: &str = if number % 2 == 0 { "even" } else { "odd" };
+
+    println!("The number is {result}")
 }
